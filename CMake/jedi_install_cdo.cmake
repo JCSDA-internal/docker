40b529e11f10031a18f0e61b2cf33e5fe0f0d151
Xin Zhang <xin.l.zhang@noaa.gov>
2017-05-08 03:44:22 +0000

    CDO is OK

--- CMake/jedi_install_cdo.cmake
+++ CMake/jedi_install_cdo.cmake
@@ -2,15 +2,14 @@
 # jedi_install_cdo
 #
 
-set(cdo_url "https://code.zmaw.de/attachments/download/14271/cdo-1.8.1.tar.gz")
+set(cdo_url "https://code.zmaw.de/attachments/download/14387/cdo-current.tar.gz")
 set(cdo_md5 "")
 ExternalProject_Add( cdo
     DOWNLOAD_DIR ${JEDI_DOWNLOADS_DIR}
     URL ${cdo_url}
     URL_MD5 ${cdo_md5}
     CONFIGURE_COMMAND
-    #<SOURCE_DIR>/configure --prefix=${JEDI_PREFIX} --with-netcdf=${JEDI_PREFIX} --with-szlib=${JEDI_PREFIX} --with-grib_api=${JEDI_PREFIX} --with-udunits2=${JEDI_PREFIX} --with-curl --without-hdf5
-    <SOURCE_DIR>/configure --prefix=${JEDI_PREFIX} -with-curl --with-szlib=${JEDI_PREFIX} --with-grib_api=${JEDI_PREFIX} --with-udunits2=${JEDI_PREFIX} --with-hdf5=${JEDI_PREFIX} --with-netcdf=${JEDI_PREFIX}
+    <SOURCE_DIR>/configure --prefix=${JEDI_PREFIX} -with-curl --with-szlib=${JEDI_PREFIX} --with-grib_api=${JEDI_PREFIX} --with-udunits2=${JEDI_PREFIX} --with-hdf5=${JEDI_PREFIX} --with-netcdf=${JEDI_PREFIX} --with-jasper=${JEDI_PREFIX}
     CC=${CMAKE_C_COMPILER}
     CXX=${CMAKE_CXX_COMPILER}
     CFLAGS=${CFLAGS}
