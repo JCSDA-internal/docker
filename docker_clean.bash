e36c2038c56056fa09e3b5aa29f88a71235f558c
xinzhang <xinzhang@jcsda-dev1.joss.ucar.edu>
2017-08-10 17:21:15 -0600

    bug fixes

--- docker_clean.bash
+++ docker_clean.bash
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -a -q)
+docker stop $(docker ps -a -q) > /dev/null 2>&1 &&  docker rm $(docker ps -a -q) &&  docker rmi $(docker images -a -q)
