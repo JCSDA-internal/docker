6eb5049297e8934f17d92d2d8af8f3a382508bf1
Xin Zhang <Xin.L.Zhang@noaa.gov>
2017-10-19 15:36:25 -0600

    clean up

--- docker_clean.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop $(docker ps -a -q) > /dev/null 2>&1 
-docker rm $(docker ps -a -q) > /dev/null 2>&1
-docker rmi -f $(docker images -a -q) > /dev/null 2>&1
-# no matter what's the return code, avoid it !
-echo $?
