99131a945cd581a21ea09eac695a7d8c79d9feb1
Xin Zhang <xinzhang@cpaessxinzhang-mpro.local>
2018-07-25 22:24:56 -0600

    enable NCO netcdf4 ; and update the key

--- CMake/jedi_install_NCO.cmake
+++ CMake/jedi_install_NCO.cmake
@@ -9,25 +9,30 @@ function ( download_build_install )
            DOWNLOAD_DIR ${JEDI_DOWNLOADS_DIR}
            URL ${nco_URL}
            URL_MD5 ${nco_MD5}
-	   CMAKE_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DCMAKE_INSTALL_PREFIX:PATH=${JEDI_PREFIX} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-           INSTALL_COMMAND ""
-	   BUILD_IN_SOURCE 1
+           CONFIGURE_COMMAND
+           <SOURCE_DIR>/configure --prefix=${JEDI_PREFIX} --enable-netcdf4 
+           CC=${CMAKE_C_COMPILER}
+           CXX=${CMAKE_CXX_COMPILER}
+           CFLAGS=${CFLAGS}
+           CXXFLAGS=${CXXFLAGS}
+           BUILD_COMMAND ${CMAKE_BUILD_TOOL}
+           INSTALL_COMMAND ${CMAKE_BUILD_TOOL} install
            LOG_DOWNLOAD 1
            LOG_CONFIGURE 1
            LOG_BUILD 1
            LOG_INSTALL 1
            )
 
-       ExternalProject_Add_Step(
-	   NCO copyExe
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncbo ${JEDI_PREFIX}/bin/ncbo
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncecat ${JEDI_PREFIX}/bin/ncecat
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncflint ${JEDI_PREFIX}/bin/ncflint
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncks ${JEDI_PREFIX}/bin/ncks
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncpdq ${JEDI_PREFIX}/bin/ncpdq
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncra ${JEDI_PREFIX}/bin/ncra
-	   COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncrename ${JEDI_PREFIX}/bin/ncrename
-	   COMMENT "nco install, copy executables to the bin"
-	   DEPENDEES install
-	   )
+#      ExternalProject_Add_Step(
+#      NCO copyExe
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncbo ${JEDI_PREFIX}/bin/ncbo
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncecat ${JEDI_PREFIX}/bin/ncecat
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncflint ${JEDI_PREFIX}/bin/ncflint
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncks ${JEDI_PREFIX}/bin/ncks
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncpdq ${JEDI_PREFIX}/bin/ncpdq
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncra ${JEDI_PREFIX}/bin/ncra
+#      COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/ncrename ${JEDI_PREFIX}/bin/ncrename
+#      COMMENT "nco install, copy executables to the bin"
+#      DEPENDEES install
+#      )
 endfunction()
