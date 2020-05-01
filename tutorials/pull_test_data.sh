0ace79f45ac5567622867fbfdccf986761a04752
Mark Miesch <miesch@ucar.edu>
2020-05-01 11:31:17 -0600

    Feature/tutorial (#32)
    * added dockerfile for tutorial
    
    * build fv3-bundle in container
    
    * Added Singularity recipe file and build script
    
    * tweak singularity recipe, add run script
    
    * Do build in container
    
    * config files for tutorial 1
    
    * renaming and added scripts to push and pull tutorial data from S3
    
    * bugfix
    
    * config files essentially identical to tests
    
    * link directly to data files in container
    
    * link to build directory for obs and crtm
    
    * bump files for 4dvar
    
    * update diffstates yaml files
    
    * add panoply to container and bugfix on diffstates filename
    
    * Add push to container build script

---
+++ tutorials/pull_test_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#----------------------------------------------------------------------
+# This script pulls the data files needed for the tutorials from S3
+#----------------------------------------------------------------------
+
+set -e
+
+# when we have more than one tutorial, make this a list and loop over it
+name=run-jedi
+
+# Get git branch
+branch=`git rev-parse --abbrev-ref HEAD`
+
+# Download from s3
+mkdir -p ${name}
+cd ${name}
+aws s3 cp s3://jedi-test-files/docker/${branch}/tutorials/${name}_Data.tar.gz ${name}_Data.tar.gz
+aws s3 cp s3://jedi-test-files/docker/${branch}/tutorials/${name}_Data.tar.gz.md5 ${name}_Data.tar.gz.md5
+
+# Check MD5 checksum
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    md5 -r ${name}_Data.tar.gz > pull-checksum.md5
+else
+    md5sum ${name}_Data.tar.gz > pull-checksum.md5
+fi
+
+if [[ pull-checksum.md5 -ne ${name}_Data.tar.gz.md5 ]]; then
+    echo "ERROR: in downloading ${name} Data files from S3"
+    exit 1
+fi
+
+# Unpack
+tar -xvzf ${name}_Data.tar.gz Data
+rm ${name}_Data.tar.gz
+rm ${name}_Data.tar.gz.md5
+rm pull-checksum.md5
