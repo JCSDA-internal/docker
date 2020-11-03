9f3fc52cca5e13d9fd3adbabb4faeb85d24a84c2
Mark Miesch <miesch@ucar.edu>
2020-11-03 16:38:53 -0700

    Feature/new dev containers (#41)
    * unlimited stack size
    
    * split up container build and push
    
    * bugfix
    
    * bugfix
    
    * update clang dockerfile
    
    * bugfix
    
    * remove tutorial dockerfile
    
    * review comments

--- build_container.sh
+++ build_container.sh
@@ -5,17 +5,6 @@
 
 
 #------------------------------------------------------------------------
-function get_ans {
-    ans=''
-    while [[ $ans != y ]] && [[ $ans != n ]]; do
-      echo $1
-      read ans < /dev/stdin
-      if [[ $ans != y ]] && [[ $ans != n ]]; then echo "You must enter y or n"; fi
-    done
-}
-
-
-#------------------------------------------------------------------------
 if [ $# -ne 1 ]; then
    echo "Usage: "
    echo "./build_container.sh <container-name>"
@@ -27,28 +16,10 @@ set -e
 
 export CNAME=${1:-"gnu-openmpi-dev"}
 
-
 #------------------------------------------------------------------------
 # Build image
 # tag it as beta for testing purposes - this will be retagged as latest
 
 docker image build --no-cache -f Dockerfile.${CNAME} -t jcsda/docker-${CNAME}:beta . 2>&1 | tee build.log
 
-#------------------------------------------------------------------------
-get_ans "Push to Docker Hub?"
-
-if [[ $ans == y ]] ; then
-
-    # save previous image in case something goes wrong
-    docker pull jcsda/docker-$CNAME:latest
-    docker tag jcsda/docker-$CNAME:latest jcsda/docker-$CNAME:revert
-    docker push jcsda/docker-$CNAME:revert
-    docker rmi jcsda/docker-$CNAME:latest
-
-    # push new image and re-tag it with latest
-    docker tag jcsda/docker-$CNAME:beta jcsda/docker-$CNAME:latest
-    docker rmi jcsda/docker-$CNAME:beta
-    docker push jcsda/docker-$CNAME:latest
-    
-fi
 #------------------------------------------------------------------------
