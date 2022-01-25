83fd9afa473f492ab6c7a34fc1c65e97f71de168
Maryam Abdi-Oskouei <maryamao@ucar.edu>
2022-01-25 15:44:14 -0700

    add comments

--- UKMO/build_container.sh
+++ UKMO/build_container.sh
@@ -3,8 +3,13 @@
 # This software is licensed under the terms of the Apache Licence Version 2.0 which can be obtained at
 # http://www.apache.org/licenses/LICENSE-2.0.
 
-
 #------------------------------------------------------------------------
+#  To build the base container run
+#  ./build_container.sh jopa-gnu-ompi-base beta
+#  Next build the ci container by running:
+#  ./build_container.sh jopa-gnu-ompi-ci beta
+#  jedi-stack@feature/jopa-container, shumlib, faux, and trans will be built in the ci container
+
 if [ $# -lt 1 ]; then
    echo "Usage: "
    echo "./build_container.sh <container-name> <tag>"
